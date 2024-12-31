57e6b97128269faa53236f004af9801d9d91ee22
bem130 <bem130@ymail.ne.jp>
2024-12-31 16:33:29 +0900

    webで、サンプルコードを別ファイルから取得するように変更

--- src/index.ts
+++ src/index.ts
@@ -11,67 +11,28 @@ function CompileAsTypescriptResult(code: string): IntermediateProducts {
     return result_from_rust;
 }
 
+async function fetchTextFile(url: string): Promise<string> {
+    try {
+        // fetchでリソースを取得
+        const response = await fetch(url);
+
+        // レスポンスが成功しているかを確認
+        if (!response.ok) {
+            throw new Error(`HTTP Error: ${response.status}`);
+        }
+
+        // テキストとしてレスポンスを取得
+        const text = await response.text();
+        return text;
+    } catch (error) {
+        console.error("テキストファイルの取得中にエラーが発生しました:", error);
+        throw error;
+    }
+}
+
 async function run() {
     await init();
-    const input = `
-// Example usage with comments
-using nor:2->1;
-
-// This is a NOT gate module
-module not (x)->(a) {
-    a: nor <- x x;
-}
-
-module or (x y)->(b) {
-    a: nor <- x y;
-    b: not <- a  ;
-}
-
-module and (x y)->(c) {
-    a: not <- x  ;
-    b: not <- y  ;
-    c: nor <- a b;
-}
-
-module xor (x y)->(e) {
-    a: not <- x  ;
-    b: not <- y  ;
-    c: nor <- a b;
-    d: nor <- x y;
-    e: nor <- c d;
-}
-
-module hAddr (x y)->(c s) {
-    c: and <- x y;
-    s: xor <- x y;
-}
-
-module fAdr (x y z)->(c s2) {
-    c1 s1: hAddr <- x y  ;
-    c2 s2: hAddr <- s1 z ;
-    c    : or    <- c1 c2;
-}
-
-test not:1->1 {
-    t -> f;
-    f -> t;
-}
-
-test or:2->1 {
-    t t -> t;
-    t f -> t;
-    f t -> t;
-    f f -> f;
-}
-
-test and:2->1 {
-    t t -> t;
-    t f -> f;
-    f t -> f;
-    f f -> f;
-}
-    `;
-    const input_without_space = `USEnor:2>1;DEFnot(x)>(a){a:nor<x,x;}DEFor(x,y)>(b){a:nor<x,y;b:not<a;}DEFand(x,y)>(c){a:not<x;b:not<y;c:nor<a,b;}DEFxor(x,y)>(e){a:not<x;b:not<y;c:nor<a,b;d:nor<x,y;e:nor<c,d;}DEFhAddr(x,y)>(c,s){c:and<x,y;s:xor<x,y;}DEFfAdr(x,y,z)>(c,s2){c1,s1:hAddr<x,y;c2,s2:hAddr<s1,z;c:or<c1,c2;}TESTnot:1>1{t>f;f>t;}TESTor:2>1{t,t>t;t,f>t;f,t>t;f,f>f;}TESTand:2>1{t,t>t;t,f>f;f,t>f;f,f>f;}`;
+    const input = await fetchTextFile("./sample.ncg");
     {
         console.log("< Input >")
         const result = CompileAsTypescriptResult(input);
