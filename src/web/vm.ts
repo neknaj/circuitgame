9d74924c00f5b50ae2f6c6eca1ac6d108da5cc61
bem130 <bem130@ymail.ne.jp>
2025-01-11 03:06:24 +0900

    GUIでのモジュール名の保存を改善

--- src/web/vm.ts
+++ src/web/vm.ts
@@ -11,12 +11,13 @@ let waveLabels = [];
 
 function init(elm: HTMLDivElement,product: IntermediateProducts,module_name: string = null) {
     if (module_name!=null&&product.module_dependency_sorted.includes(module_name)==false) { module_name = null; }
-    if (selected!=null&&product.module_dependency_sorted.includes(selected)==false) { selected = null; }
     if (module_name == null) {
         if (selected == null) { selected = product.module_dependency_sorted[0]; }
         if (selected == null) { return; }
         module_name = selected;
     }
+    if (module_name!=null&&product.module_dependency_sorted.includes(module_name)==false) { return; }
+    selected = module_name;
     const binary = Compile(product.source,module_name);
     if (binary.length==0) { console.warn("compiling error") ;return; }
     vm_id = VM.init(binary);
